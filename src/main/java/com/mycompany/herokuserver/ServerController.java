2c5152dd7165c70321b749b42a7a374ee754055a
vcaf1 <victoriafongvicky@hotmail.com>
2020-12-10 14:59:37 +0100

    created a postmapping to show the daa from the databse

--- src/main/java/com/mycompany/herokuserver/ServerController.java
+++ src/main/java/com/mycompany/herokuserver/ServerController.java
@@ -8,6 +8,7 @@ package com.mycompany.herokuserver;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -33,13 +34,34 @@ public class ServerController {
     @Autowired
     private LuchtModuleDAO luchtModuleDao;
     private ArrayList<LuchtModule> lijst = new ArrayList<>();
+    private LuchtModules list = new LuchtModules();
 
     //private DataSource dataSource;
     private int counter = 0;
 
     @GetMapping(path = "/", produces = "application/json")
     public LuchtModules getLuchtModules() {
-        return luchtModuleDao.getAllLuchtModules();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Connection con = DBCPDataSource.getConnection();
+            Statement stat = con.createStatement();
+            ResultSet result = stat.executeQuery("select * from luchtmodules");
+            while (result.next()) {
+                LuchtModule dbluchtmodule = new LuchtModule(result.getInt("lumod_id"),result.getInt("temperatuur"), result.getInt("vochtigheid"));
+                list.getLuchtModuleList().add(dbluchtmodule);
+            }
+            System.out.println("Added the data from the ElephantSQL databse from the Luchtmodule");
+            }catch (SQLException se) {
+                System.out.println(se.getMessage());
+            }
+        
+        return list;
     }
 
     @PostMapping(path = "/kpn", produces = "application/json")
