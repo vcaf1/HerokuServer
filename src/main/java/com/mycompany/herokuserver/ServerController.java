c43c88017e83eac3bb64070b5a5fb9818b12ad1b
jvhuffele <j.vanhuffelen@student.avans.nl>
2021-01-19 16:27:44 +0100

    changed bodemmodule table for test with date

--- src/main/java/com/mycompany/herokuserver/ServerController.java
+++ src/main/java/com/mycompany/herokuserver/ServerController.java
@@ -477,7 +477,7 @@ public class ServerController {
                 Connection con = DBCPDataSource.getConnection();
                 Statement stat = con.createStatement();
 
-                String insertStatementBodem = "insert into smartfarm.soildata (soil_value) values('" +  dbBodemmodule.getValueSoil()+ "')";
+                String insertStatementBodem = "insert into smartfarm.soilmodules (soil_value,created_at) values('" + dbBodemmodule.getValueSoil()+"', '')";
                 int resultT = stat.executeUpdate(insertStatementBodem);
 
                 int moduleSoilmoisture = bodemlijst.get(bodemcounter).getValueSoil();
